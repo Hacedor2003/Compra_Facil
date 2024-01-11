0c65e37f76641b10f6afb9bb642e5a39f53a6452
Hacedor2003 <bryanlenier@gmail.com>
2024-01-11 06:15:25 -0500

    Optimizacion

--- src/App/Page/Product/Routes/ListaProductos/Components/Producto/Components/Valoracion/Valoracion.jsx
+++ src/App/Page/Product/Routes/ListaProductos/Components/Producto/Components/Valoracion/Valoracion.jsx
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Valoracion = ({ rating, setRating }) => {
-  const [valoracion, setValoracion] = useState(rating);
+	const [valoracion, setValoracion] = useState(rating);
 
-  const handleStarClick = (value) => {
-    if (valoracion === value) {
-      setValoracion(0); // Quits the rating if clicked on the same star
-      setRating(0);
-    } else {
-      setValoracion(value);
-      setRating(value);
-    }
-  };
+	const handleStarClick = (value) => {
+		if (valoracion === value) {
+			setValoracion(0);
+			setRating(0);
+		} else {
+			setValoracion(value);
+			setRating(value);
+		}
+	};
 
-  return (
-    <div>
-      {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
+	return (
+		<div>
+			{[...Array(5)].map((star, i) => {
+				const ratingValue = i + 1;
 
-        return (
-          <span key={i}>
-            <label
-              role='img'
-              aria-label='star'
-              onClick={() => handleStarClick(ratingValue)}
-              style={{ cursor: 'pointer' }}>
-              {ratingValue <= valoracion ? '⭐️' : '☆'}
-            </label>
-          </span>
-        );
-      })}
-    </div>
-  );
+				return (
+					<span key={i}>
+						<label
+							role='img'
+							aria-label='star'
+							onClick={() => handleStarClick(ratingValue)}
+							style={{ cursor: 'pointer' }}>
+							{ratingValue <= valoracion ? '⭐️' : '☆'}
+						</label>
+					</span>
+				);
+			})}
+		</div>
+	);
 };
 
 Valoracion.propTypes = {
-  rating: PropTypes.number.isRequired,
-  setRating: PropTypes.func.isRequired,
+	rating: PropTypes.number.isRequired,
+	setRating: PropTypes.func.isRequired,
 };
 
 export default Valoracion;
