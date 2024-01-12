09b503c5381a8ec4501fc931d21f72930919d0f3
Hacedor2003 <bryanlenier@gmail.com>
2024-01-11 21:19:25 -0500

    Prueba PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './Data/Store/Store';
-import { Provider } from 'react-redux';
-import { Index } from './App/index';
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./Data/Store/Store";
+import { Provider } from "react-redux";
+import { Index } from "./App/index";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-			<Provider store={store}>
-				<Index />
-			</Provider>
-	</React.StrictMode>
+registerSW({ immediate: true });
+
+const updateSW = registerSW({
+    onOfflineReady() {},
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Index />
+        </Provider>
+    </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,gif,png,svg}"],
+      },
+      includeAssets: [
+        "./src/assets/search.gif",
+        "./src/assets/_defd7984-1068-474a-95bd-422bdcc.jpg",
+        "./src/assets/icons8-web-24.png",
+      ],
+      manifest: {
+        name: "Compra Facil",
+        short_name: "CompraFast",
+        description: "Pagina Web de e-commerce falsa",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./src/assets/icons8-shop-100.png",
+            sizes: "100x100",
+            type: "image/png",
+          },
+          {
+            src: "./src/assets/store-4156934_640.png",
+            sizes: "640x631",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
